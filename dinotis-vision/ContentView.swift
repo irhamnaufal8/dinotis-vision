20adfec0334168b59e2d4991549a0219fe2ed245
Irham Naufal <inaufal77@gmail.com>
2024-03-27 23:06:50 +0700

    Initial Commit

---
+++ dinotis-vision/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  dinotis-vision
+//
+//  Created by Irham Naufal on 27/03/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ dinotis-vision/dinotis_visionApp.swift
@@ -0,0 +1,17 @@
+//
+//  dinotis_visionApp.swift
+//  dinotis-vision
+//
+//  Created by Irham Naufal on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct dinotis_visionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
